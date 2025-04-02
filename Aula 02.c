cf4151ff6913be2845dcb15adc31e32d9e5e6494
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-04-02 08:53:38 -0300

    Update Aula 02.c

--- Aula 02.c
+++ Aula 02.c
@@ -13,7 +13,6 @@ int main()
     if(h>1 || m >15)
     {
         s= (h*3600)+(m*60)+s;
-
         printf("Quantidade de segundos: %f\n", s);
     }
 
@@ -21,7 +20,6 @@ int main()
     else
     {
         m=(h*60)+m;
-
         printf("Quantidade de minutos: %f\n", m);
     }
 
