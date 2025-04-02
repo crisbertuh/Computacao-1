2f8ce255929191439fc2157d15a97d9f6f7e6ddd
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-04-02 08:48:47 -0300

    Add files via upload

---
+++ Aula 02.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int h;
+    float m, s;
+
+    printf("Digite hora, minuto e segundo \n");
+    scanf("%d %f %f", &h, &m, &s);
+
+    //Se hora for maior que 1 e minuto maior que 15
+    if(h>1 || m >15)
+    {
+        s= (h*3600)+(m*60)+s;
+
+        printf("Quantidade de segundos: %f\n", s);
+    }
+
+    //Caso o contrário
+    else
+    {
+        m=(h*60)+m;
+
+        printf("Quantidade de minutos: %f\n", m);
+    }
+
+    return 0;
+}
