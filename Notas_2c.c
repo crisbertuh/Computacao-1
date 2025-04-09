bb3c351157608ff04e6cab0c762afb5b186e7f06
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-04-09 00:04:34 -0300

    Update Notas_2c.c

--- Notas_2c.c
+++ Notas_2c.c
@@ -2,50 +2,54 @@
 
 void main()
 {
-    float media=0, n1, n2. n3;
-    int pres=0;
-    int erro=0;
+    float media=0, n1, n2, n3;
+    int pres=0, erro=0;
 
   //Leitura das notas.
   printf("Digite as 3 notas do aluno\n");
-  scanf("%f %f %f", &n1, n2, n3);
+  scanf("%f %f %f", &n1, &n2, &n3);
 
   //Se as notas forem maior que 10.0.
-  if((n1<10.0) || (n2>10.0) || (n3>10.0))
+  if((n1>10.0) || (n2>10.0) || (n3>10.0))
   {  
     printf("ERRO!! Digite 3 números menores que 10.0\n");
     erro=1;
   }
 
   //Leitura da presença.
-  printf("Digite a presença do aluno em valor de 0 a 100\n");
-  scanf("%f", pres);
+  if(erro==0)
+  {  
+      printf("Digite a presença do aluno em valor de 0 a 100\n");
+      scanf("%d", &pres);
 
-  //Se a presença for maior que 10.0.
+      //Se a presença for maior que 10.0.
   if(pres>100)
   {  
     printf("ERRO!! Digite um numero menor que 100.0\n");
     erro=1;
   }
   //Se a presença for insuficiente.
-  if(pres<75)
+  if(pres<=50)
   {
-    printf("Aluno reprovado!"\n)
+    printf("Aluno reprovado direto!\n");
+    erro=1;
   }
-
+  }
+  
   //Se não houver nenhum erro, faça os cáculos e imprima os valores.
-  if((erro==0) && (pres<=75)) 
+  if(erro==0) 
   {
     media=(n1+n2+n3)/3.0;
     
-    if(media>=8.0)
-      printf("Aprovado com Distinção! \nSua Média é: %f \nSua presença é: %f ", media, pres);
-    else if(media=<7.50 && media=>6.0)
-      printf("Aprovado Direto" \nSua Média é: %f \nSua presença é: %f ", media, pres");
-    else if(media=<6.0 && media=>4.0)
-      printf("Vai para Final" \nSua Média é: %f \nSua presença é: %f ", media, pres");
-    else if(media=<4.0)
-      printf("Reprovado Direto" \nSua Média é: %f \nSua presença é: %f ", media, pres");
+    if((media>=8.0) && (pres>=75))
+      printf("Aprovado com Distinção! \nSua Média é: %f \nSua presença é: %d ", media, pres);
+    else if( (media<=7.50) && (media>=6.0) && (pres>=75))
+      printf("Aprovado Direto \nSua Média é: %f \nSua presença é: %d ", media, pres);
+    else if( ((media<=6.0) && (media>=4.0) && (pres<=75)) || ((media>=6.0) && (pres<=75)) || ((media>=4.0) && (pres<=75)) )
+      printf("Vai para Final \nSua Média é: %f \nSua presença é: %d ", media, pres);
+    else if((media<=4.0))
+      printf("Reprovado Direto \nSua Média é: %f \nSua presença é: %d ", media, pres);
   }
   
 }
+
