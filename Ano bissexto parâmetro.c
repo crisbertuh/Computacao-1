40c12023d07ba0a1339bbee222813071782db840
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-05-07 09:36:27 -0300

    Update Ano bissexto parâmetro.c

--- Ano bissexto parâmetro.c
+++ Ano bissexto parâmetro.c
@@ -1,7 +1,6 @@
 //Cristiano Bertulino
 #include <stdio.h>
 int eh_bissexto(int ano);
-int a;
 
 void main()
 {//------Declaração das variáveis----
@@ -11,7 +10,7 @@ void main()
   scanf("%d", &ano);
   eh_bissexto(ano);
  //-----------------------------
-  if(a==1)
+  if(eh_bissexto)
     printf("Este ano é bissexto");
   else 
     printf("Este ano não é bissexto");
@@ -20,9 +19,10 @@ void main()
 int eh_bissexto(int ano)
 {
   if((ano%4==0) && (ano%100!=0 || ano%400==0))
-    a=1;
+    ano=1;
   else 
-    a=0;
+    ano=0;
   
-  return (a);
+  return (ano);
 }
+
