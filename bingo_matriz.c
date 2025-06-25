eb3f970581d9b9999ad06d56c1dfe5cc4dbd4e7f
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-06-24 23:58:57 -0300

    Update bingo_matriz.c

--- bingo_matriz.c
+++ bingo_matriz.c
@@ -1,68 +1,94 @@
+//Cristiano bertulino
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define N 5
-#define MAX_COL 100
+#define MAX_COL 100  // Define um máximo para colunas
 
-void preenche_bingo(int matriz[N][MAX_COL], int col);
+void preenche_bingo(int m[][MAX_COL], int lin, int col);
+void imprime_matriz(int m[][MAX_COL], int lin, int col);
 
-int main() {
-    int col;
-    int m[N][MAX_COL];
+int main(){
+    int lin, col;
 
-    printf("Digite o número de colunas da matriz: ");
+    printf("Quantas linhas deseja ter no bingo?\n");
+    scanf("%d", &lin);
+    printf("Quantas colunas deseja ter no bingo?\n");
     scanf("%d", &col);
 
-    if (col <= 0 || col > MAX_COL) {
-        printf("Número de colunas inválido.\n");
+    if (col > MAX_COL) {
+        printf("Número máximo de colunas é %d\n", MAX_COL);
         return 1;
     }
 
-    if (N * col > 100) {
-        printf("Matriz muito grande para números únicos entre 0 e 99.\n");
-        return 1;
-    }
+    int m[lin][MAX_COL];  // Cria matriz com linhas definidas e colunas fixas MAX_COL
 
-    preenche_bingo(m, col);
-
-    printf("Valores da sua cartela de bingo preenchida:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < col; j++) {
-            printf("%2d ", m[i][j]);
-        }
-        printf("\n");
-    }
+    preenche_bingo(m, lin, col);
+    imprime_matriz(m, lin, col);
 
     return 0;
 }
 
-void preenche_bingo(int matriz[N][MAX_COL], int col) {
+void preenche_bingo(int m[][MAX_COL], int lin, int col){
+    int num, repetido, x, y;
     srand(time(NULL));
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < col; j++) {
-            int num;
-            int repetido;
-
+    for(int i=0; i<lin; i++){
+        for(int j=0; j<col; j++){
             do {
                 num = rand() % 100;
                 repetido = 0;
 
-                // Verifica todos os elementos já preenchidos para evitar repetição
-                for (int x = 0; x < i || (x == i && x <= j); x++) {
-                    for (int y = 0; y < col; y++) {
-                        if (x == i && y >= j) break; // não verificar posições ainda não preenchidas
-                        if (matriz[x][y] == num) {
+                for(x=0; x<i || (x==i && x<j); x++){
+                    for(y=0; y<col; y++){
+                        if(x==i && y>=j) break;
+                        if(m[x][y] == num){
                             repetido = 1;
                             break;
                         }
                     }
-                    if (repetido) break;
+                    if(repetido) break;
                 }
-            } while (repetido);
+            } while(repetido == 1);
 
-            matriz[i][j] = num;
+            m[i][j] = num;
         }
     }
+
+    printf("Cartela de bingo sorteada!\n");
 }
+
+void imprime_matriz(int m[][MAX_COL], int lin, int col){
+    for(int i=0; i<lin; i++){
+        for(int j=0; j<col; j++){
+            printf("%3d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
